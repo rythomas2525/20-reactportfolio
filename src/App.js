64807f626531c2c14dd5f2d373ed522b371ad911
Ryan Thomas <rythomas2525@gmail.com>
2020-05-12 09:05:39 -0600

    added read me

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
-import Col from "./components/Col"
 import Container from "./components/Container"
-import Row from "./components/Row"
 import Footer from './components/Footer';
 import Overlay from './components/Overlay';
-import Projectslide from './components/Projectslide';
-import Aboutmetext from './components/Aboutmetext';
-import Aboutmeimage from './components/Aboutmeimage';
 import Home from "./pages/home"
-import Resumepage from "./pages/Resumepage"
+
 
 function App() {
   return (
@@ -20,11 +15,8 @@ function App() {
         <Navbar />
         <Container >
           <Route exact path="/20-reactportfolio" component={Home} />
-          <Route exact path="/20-reactportfolio/resume" component={Resumepage} />
         </Container>
-
         <Footer />
-
       </div>
     </Router >
   )
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,20 +1,15 @@
 import React from "react";
 import resumelogo from "./document-2-128.png"
 import "./style.css"
-import { Link } from "react-router-dom";
+
+
+
 
 function Resume() {
     return <div >
-        <Link to="/20-reactportfolio/resume"   ><img src={resumelogo} className="bottomicons resume " /></Link>
+        <a href="images/RyanThomasResume.pdf" download="ryansresume" target="_blank" title="Download Resume"><img src={resumelogo} alt="resumelogo" className="bottomicons resume " /></a>
     </div>
 }
-
-
-// function Resume() {
-//     return <div >
-//         <a href="images/RyanThomasResume.pdf" download="ryansresume" target="_blank" title="Download Resume"><img src={resumelogo} className="bottomicons resume " /></a>
-//     </div>
-// }
 
 
 
