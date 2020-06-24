afc1fcbe585e1a02ffd78b3630964a51b79946c7
Ryan Thomas <rythomas2525@gmail.com>
2020-06-24 12:10:16 -0600

    fixed resume button

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import resumelogo from "./document-2-128.png"
 import "./style.css"
+import resume from "./RyanThomasResume.pdf"
 
 
 
 
 function Resume() {
     return <div >
-        <a href="images/RyanThomasResume.pdf" download="ryansresume" target="_blank" title="Download Resume"><img src={resumelogo} alt="resumelogo" className="bottomicons resume " /></a>
+        <a href={resume} download="ryansresume.pdf" target="_blank" title="Download Resume"><img src={resumelogo} alt="resumelogo" className="bottomicons resume " /></a>
     </div>
 }
 
